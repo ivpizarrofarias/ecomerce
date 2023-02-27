9500aa397dbf480089404372faf9ae359207c3f5
ivpizarrofarias <75133941+ivpizarrofarias@users.noreply.github.com>
2023-02-27 16:55:29 -0300

    redireccionamiento al carrito de compras y suma de cantidades, ventas

--- src/main/java/com/ipf/software/controller/HomeController.java
+++ src/main/java/com/ipf/software/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ipf.software.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -9,8 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ipf.software.model.DetalleOrden;
+import com.ipf.software.model.Orden;
 import com.ipf.software.model.Producto;
 import com.ipf.software.service.ProductoService;
 
@@ -23,6 +29,11 @@ private final Logger log = LoggerFactory.getLogger(HomeController.class);
 	@Autowired
 	private ProductoService productoService;
 	
+	//pa alamacenar los detalle de la orden
+	List<DetalleOrden> detalles= new ArrayList<DetalleOrden>();
+	
+	// datos de la  orden
+	Orden orden = new Orden();
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -40,5 +51,32 @@ private final Logger log = LoggerFactory.getLogger(HomeController.class);
 		model.addAttribute("producto",producto);
 		
 		return "usuario/productohome";
+	}
+	
+	@PostMapping("/cart")
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
+		
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto = new Producto();
+		double sumaTotal = 0;
+		
+		Optional<Producto> optionalProducto = productoService.get(id);
+		log.info("Producto añadido al carro: {}", optionalProducto.get());
+		log.info("Cantidad: {}", cantidad);
+		producto = optionalProducto.get();
+		
+		detalleOrden.setCantidad(cantidad);
+		detalleOrden.setPrecio(producto.getPrecio());
+		detalleOrden.setNombre(producto.getNombre());
+		detalleOrden.setTotal(producto.getPrecio()*cantidad);
+		detalleOrden.setProducto(producto);
+		detalles.add(detalleOrden);
+		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		
+		return "usuario/carrito";
 	}
 }
