788a7f8174233625b17099c0749969c79f97c3f5
ivpizarrofarias <75133941+ivpizarrofarias@users.noreply.github.com>
2023-02-27 17:17:27 -0300

    funcionalidad delete carrito

--- src/main/java/com/ipf/software/controller/HomeController.java
+++ src/main/java/com/ipf/software/controller/HomeController.java
@@ -79,4 +79,29 @@ private final Logger log = LoggerFactory.getLogger(HomeController.class);
 		
 		return "usuario/carrito";
 	}
+	
+	// quitar un producto del carrito
+	
+	@GetMapping("/delete/cart/{id}")
+	public String deleteProductocart(@PathVariable Integer id, Model model) {
+		
+		// nueva lista de productos
+		List<DetalleOrden> ordenesNueva= new ArrayList<DetalleOrden>();
+		
+		for (DetalleOrden detalleOrden : detalles) {
+			if (detalleOrden.getProducto().getId()!=id) {
+				ordenesNueva.add(detalleOrden);
+			}
+		}
+		//poner nueva lista con productos restantes
+		detalles=ordenesNueva;
+		double sumaTotal=0;
+		
+		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		return "usuario/carrito";
+	}
 }
